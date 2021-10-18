2f45ca698e9adf0feecb2e3f1ea0aff81698cb78
Fahim Faisal <fahim.faisal.abir@gmail.com>
2021-10-18 17:16:24 +0600

    Fixed some issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ubuntu: "'Ubuntu', sans-serif"
+      }
+    },
   },
   variants: {
     extend: {},
